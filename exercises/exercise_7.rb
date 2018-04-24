85ba95db036e565717b87016139bb0717f26ddd2
Phil Werner <philwerner@shaw.ca>
2018-04-24 23:45:00 +0000

    finished exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,13 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
+puts "Enter a new store name"
+new_store_name = gets
+#puts new_store_name
+
+@another_store = Store.create(name: "#{new_store_name}")
+
+puts @another_store.errors.messages
+
+
 # Your code goes here ...
